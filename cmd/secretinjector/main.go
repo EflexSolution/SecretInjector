e32237ac251fb9feff63b01f01bb01c62af37107
Efle <152645371+EflexSolution@users.noreply.github.com>
2025-12-10 19:58:50 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/secretinjector/main.go
@@ -0,0 +1,23 @@
+// cmd/secretinjector/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "secretinjector/internal/secretinjector"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := secretinjector.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
